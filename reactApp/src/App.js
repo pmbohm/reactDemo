eac3a4d051811e23df1f6c90d13e88c3ce845575
pmbohm <pmbohm@utas.edu.au>
2019-12-11 10:31:39 +1100

    move files

---
+++ reactApp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'semantic-ui-less/semantic.less'
+import DesktopContainer from "./layouts/Homepage";
+
+function App() {
+  return (
+    <div className="App">
+        <DesktopContainer />
+    </div>
+  );
+}
+
+export default App;
