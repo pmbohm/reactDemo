cd1cdd9788c6a5751223160f7c964906071d44c4
pmbohm <pmbohm@utas.edu.au>
2019-12-11 16:47:03 +1100

    Broken Tomcat deployment java HelloWorld.java

---
+++ src/main/java/demo/helloworld/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package demo.helloworld.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class HelloWorld {
+  @GetMapping("/api/hello")
+  public String hello() {
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+    final LocalDateTime now = LocalDateTime.now();
+
+    return ("Hello, World! The current time is " + dtf.format(now));
+  }
+
+}
