41ca15da154125a1a3cf9978c53d45192297e489
pmbohm <pmbohm@utas.edu.au>
2019-12-18 09:27:53 +1100

    Change to use react-bootstrap with sass

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Container from "react-bootstrap/Container";
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- reactApp/src/pages/react-bootstrap-test.js
+++ reactApp/src/pages/react-bootstrap-test.js
@@ -23,6 +23,31 @@ class ReactBootstrapTest extends Component {
                 </Row>
                 </Jumbotron>
 
+                <table data-toggle="table">
+                    <thead>
+                    <tr>
+                        <th>Item ID</th>
+                        <th>Item Name</th>
+                        <th>Item Price</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Item 1</td>
+                        <td>$1</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Item 2</td>
+                        <td>$2</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+
+                <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.15.5/dist/bootstrap-table.min.css">
+
             </Container>
 
 
